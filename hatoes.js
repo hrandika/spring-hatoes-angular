a3ce8c844ef49dbe8490168ea43323ab65b785ac
Randika Hapugoda <hrandika@hotmail.com>
2015-08-12 12:45:13 +0530

    refactors

--- hatoes.js
+++ hatoes.js
@@ -1,7 +1,9 @@
 angular.module('spring.hatoes', []).factory('hatoes', ['$http', '$log', 'ngToast',
     function($http, $log, ngToast) {
         var rest = {};
+        //
         // Find All
+        //
         rest.findAll = function(url, objectType, para) {
             return $http.get(url, {
                 params: para
@@ -27,15 +29,11 @@ angular.module('spring.hatoes', []).factory('hatoes', ['$http', '$log', 'ngToast
                 };
             });
         };
-        rest.findAllIn = function(obj, objectType, objectName, para) {
-            var paraStart = obj._links.self.href.indexOf("{?");
-            var url = null;
-            if (paraStart > 0) {
-                url = obj._links.self.href.substring(0, paraStart);
-            } else {
-                url = obj._links.self.href;
-            }
-            return $http.get(url + '/' + objectType, {
+        //
+        // Find All in
+        //
+        rest.findAllIn = function(object, objectType, objectName, para) {
+            return $http.get(removeParams(object) + '/' + objectType, {
                 params: para
             }).then(function(received) {
                 if (received.data._embedded) {
@@ -48,6 +46,9 @@ angular.module('spring.hatoes', []).factory('hatoes', ['$http', '$log', 'ngToast
                 }
             });
         };
+        //
+        // Find One
+        //
         rest.findOne = function(url, id, para) {
             return $http.get(url + "/" + id, {
                 params: para
@@ -55,7 +56,9 @@ angular.module('spring.hatoes', []).factory('hatoes', ['$http', '$log', 'ngToast
                 return received.data;
             });
         };
+        //
         // Save
+        //
         rest.save = function(url, data) {
             return $http.post(url, data).then(function(received) {
                 var myToastMsg = ngToast.success({
@@ -71,16 +74,12 @@ angular.module('spring.hatoes', []).factory('hatoes', ['$http', '$log', 'ngToast
                 return error;
             });
         };
+        //
+        //  Save with List
+        //
         rest.saveWithList = function(url, data, listType, list) {
             return $http.post(url, data).then(function(received) {
-                var paraStart = received.data._links.self.href.indexOf("{?");
-                var url = null;
-                if (paraStart > 0) {
-                    url = received.data._links.self.href.substring(0, paraStart);
-                } else {
-                    url = received.data._links.self.href;
-                }
-                $http.patch(url + "/" + listType, list.join('\n'), {
+                $http.patch(removeParams(received) + "/" + listType, list.join('\n'), {
                     headers: {
                         'Content-type': 'text/uri-list'
                     }
@@ -99,15 +98,11 @@ angular.module('spring.hatoes', []).factory('hatoes', ['$http', '$log', 'ngToast
                 return error;
             });
         };
-        rest.patch = function(obj) {
-            var paraStart = obj._links.self.href.indexOf("{?");
-            var url = null;
-            if (paraStart > 0) {
-                url = obj._links.self.href.substring(0, paraStart);
-            } else {
-                url = obj._links.self.href;
-            }
-            return $http.patch(url, obj).then(function(received) {
+        //
+        // Patch
+        //
+        rest.patch = function(object) {
+            return $http.patch(removeParams(object), object).then(function(received) {
                 var myToastMsg = ngToast.info({
                     content: 'Successfully updated.',
                     timeout: 5000,
@@ -116,15 +111,11 @@ angular.module('spring.hatoes', []).factory('hatoes', ['$http', '$log', 'ngToast
                 return received;
             });
         };
+        //
+        // Patch with list
+        //
         rest.patchWithList = function(object, dataType, list) {
-            var paraStart = object._links.self.href.indexOf("{?");
-            var url = null;
-            if (paraStart > 0) {
-                url = object._links.self.href.substring(0, paraStart);
-            } else {
-                url = object._links.self.href;
-            }
-            return $http.patch(url + '/' + dataType, list.join('\n'), {
+            return $http.patch(removeParams(object) + '/' + dataType, list.join('\n'), {
                 headers: {
                     'Content-type': 'text/uri-list'
                 }
@@ -136,15 +127,11 @@ angular.module('spring.hatoes', []).factory('hatoes', ['$http', '$log', 'ngToast
                 return received2;
             });
         };
-        rest.delete = function(obj) {
-            var paraStart = obj._links.self.href.indexOf("{?");
-            var url = null;
-            if (paraStart > 0) {
-                url = obj._links.self.href.substring(0, paraStart);
-            } else {
-                url = obj._links.self.href;
-            }
-            return $http.delete(url).then(function(received) {
+        //
+        // Delete
+        //
+        rest.delete = function(object) {
+            return $http.delete(removeParams(object)).then(function(received) {
                 var myToastMsg = ngToast.danger({
                     content: 'Deleted.',
                     timeout: 3000,
@@ -152,8 +139,11 @@ angular.module('spring.hatoes', []).factory('hatoes', ['$http', '$log', 'ngToast
                 return received;
             });
         };
-        rest.deleteFromObject = function(obj, dataType, id) {
-            var url = obj._links.self.href + '/' + dataType + '/' + id;
+        //
+        // Delete From Object
+        //
+        rest.deleteFromObject = function(object, dataType, id) {
+            var url = removeParams(object) + '/' + dataType + '/' + id;
             return $http.delete(url).then(function(received) {
                 var myToastMsg = ngToast.danger({
                     content: 'Removed.',
@@ -162,16 +152,21 @@ angular.module('spring.hatoes', []).factory('hatoes', ['$http', '$log', 'ngToast
                 return received;
             });
         };
+        //
         // Search a Single Object by Given Parameters
+        //
         rest.seachForOne = function(url, para, objectType) {
             return $http.get(url, {
                 params: para
-            }).then(function(received) {                
+            }).then(function(received) {
                 if (received.data._embedded) {
                     return received.data._embedded[objectType][0]
                 }
             });
         };
+        //
+        // Search One
+        //
         rest.searchOne = function(url, para, dataType, returnDatatype, position) {
             return $http.get(url, {
                 params: para
@@ -212,6 +207,20 @@ angular.module('spring.hatoes', []).factory('hatoes', ['$http', '$log', 'ngToast
                 });
             });
         };
+        //
+        // Utils
+        //
+        var removeParams = function(object) {
+            var paraStart = object._links.self.href.indexOf("{?");
+            var url = null;
+            if (paraStart > 0) {
+                url = object._links.self.href.substring(0, paraStart);
+            } else {
+                url = object._links.self.href;
+            }
+            return url;
+        };
+        // Return Object 
         return rest;
     }
 ]);
